3c5ebb6bf81e35547e2fc9fddf215dbf0fe4b093
cristian vega <cris02vega@gmail.com>
2025-03-02 18:04:34 -0600

    feat: modificar clase principal para los cursos de logica

---
+++ Pruebas_JUnit/src/main/java/com/cristian/www/isUnique.java
@@ -0,0 +1,12 @@
+package com.cristian.www;
+
+public class isUnique {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+
+    public static boolean isUnique(String str) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
