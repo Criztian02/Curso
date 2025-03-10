c003d5f879a1773d167e266ebd92a54ca68c1706
cristian vega <cris02vega@gmail.com>
2025-03-09 18:46:38 -0600

    test: Agregar nuevo test para saldo y descipcion de cada test

--- Pruebas_JUnit/src/test/java/com/cristian/www/modules/CuentaTest.java
+++ Pruebas_JUnit/src/test/java/com/cristian/www/modules/CuentaTest.java
@@ -9,6 +9,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CuentaTest {
 
+    /**
+     * Verifica que el saldo de la cuenta se establece correctamente
+     * al crear una instancia de la clase Cuenta.
+     */
+    @Test
+    void testSaldoCuenta() {
+        Cuenta cuenta = new Cuenta("Cristian", new BigDecimal("1000.12345"));
+        assertEquals(new BigDecimal("1000.12345"), cuenta.getSaldo());
+    }
+
+    /**
+     * Verifica que el nombre de la cuenta se establece correctamente
+     * al crear una instancia de la clase Cuenta.
+     */
     @Test
     void testNombreCuenta() {
         Cuenta cuenta = new Cuenta("Cristian", new BigDecimal("1000.12345"));
